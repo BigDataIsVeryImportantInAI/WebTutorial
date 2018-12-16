8b84aa0d46d095a7e4dc23b2a7cf03bc7ab5773f
EunGi <znfks8819@gmail.com>
2018-12-17 02:01:31 +0900

    js 파일 생성, 객체까지 완성

---
+++ color.js
@@ -0,0 +1,36 @@
+var Links = {
+    setColor : function(color) {
+        var aList = document.querySelectorAll('a');
+        var i = 0;
+        while(i < aList.length) {
+            aList[i].style.color = color;
+            i++;
+        }
+    }
+}
+var Body = {
+    setColor : function(color) {
+        var bodyStyle = document.querySelector('body').style;
+        bodyStyle.color = color;
+    },
+    setBackgroundColor : function(color) {
+        var bodyStyle = document.querySelector('body').style;
+        bodyStyle.backgroundColor = color;
+    },
+}
+function dayAndNightModeSwitch(self) {
+    var bodyStyle = document.querySelector('body').style;
+    if (self.value == 'night') {
+        self.value = 'day';
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        
+        Links.setColor('powderblue');
+    } else {
+        self.value = 'night';
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        
+        Links.setColor('blue');
+    }
+}
